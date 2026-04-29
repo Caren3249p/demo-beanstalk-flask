23e33814fc0708c9cfa67c7730fdb034ee1251e8
caren julieth diaz claderon <124204861+Caren3249p@users.noreply.github.com>
2026-04-29 16:28:34 -0500

    Update application.py

--- application.py
+++ application.py
@@ -4,8 +4,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def home():
-    return "Flask funcionando en Elastic Beanstalk - version 1"
-
+    return "Flask funcionando en Elastic Beanstalk - version 2"
 @application.route("/health")
 def health():
     return {"status": "ok"}
